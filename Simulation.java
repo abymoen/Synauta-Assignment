7ff0a8f9bde4b4fb93ed850111ba1781dc643664
Alex Bymoen <a.bymoen@hotmail.com>
2018-04-03 20:24:18 -0600

    Added variables for tracking movement and 'turns'

--- Simulation.java
+++ Simulation.java
@@ -4,12 +4,24 @@ public class Simulation {
 
   private Scanner inputScanner;
 
+  // These integer arrays denote movement in the x and y direction based on the direction currently facing (N,E,S,W)
+  private int[] xMovements;
+  private int[] yMovements;
+  // These integers will track the robots current x and y position
+  private int currentX;
+  private int currentY;
+
   public Simulation() {
-    
+    this.inputScanner = new Scanner(System.in);
+    this.xMovements = new int[]{0,1,0,-1};
+    this.yMovements = new int[]{1,0,-1,0};
   }
 
   public void startSimulation() {
-
+    clearGarbageInput();
   }
 
+  public void clearGarbageInput() {
+
+  }
 }
